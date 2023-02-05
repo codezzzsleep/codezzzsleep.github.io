7ee69ef4f6bc15acf470c4fe7640dcc2ad83fe81
codezzzsleep <codezzzsleep@163.com>
2023-02-05 21:02:57 +0800

    update

--- auto-git.bat
+++ auto-git.bat
@@ -1,7 +1,7 @@
 @REM echo "Start submitting code to the local repository"
 @REM echo "The current directory is：%cd%"
 git add *
-echo;
+@REM echo;
  
  
  
@@ -11,11 +11,11 @@ echo;
 @REM set now=%date% %time%
 @REM echo %now%
 git commit -m "update"
-echo;
+@REM echo;
  
 @REM echo "Commit the changes to the remote git server"
 git push
-echo;
+@REM echo;
  
 @REM echo "Batch execution complete!"
 @REM echo;
